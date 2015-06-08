43e585d773c556f1cffacca712031ed3f4eedc2c
fohnwind <fohn78@gmail.com>
2015-06-08 23:27:48 +0800

    add ping tools 20150608

---
+++ sh/ping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for ip in `cat hosts`
+do
+		ping -c2 $ip > /dev/null 2>&1
+		if [ $? -eq 0 ];then
+				echo -e "\033[32m $ip is up.\033[0m"
+		else
+				echo -e "\033[31m $ip is down. \033[0m"
+		fi
+done
