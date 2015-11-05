caaa8258a71102f75b6c08cdaed60a7fb2bde0e9
fohnwind <fohn78@gmail.com>
2015-11-05 11:01:16 +0800

    update checkping

--- sh/ping.sh
+++ sh/ping.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 for ip in `cat hosts`
 do
-	ping -c 2 -i 0.5 $ip > /dev/null 2>&1
-	if [ $? -eq 0 ];then
-		echo -e "\033[32m $ip is up.\033[0m"
-	else
-		echo -e "\033[31m $ip is down. \033[0m"
-	fi
+    ping -c 1 -w 1 -i 0.2 $ip &> /dev/null
+    if [ $? -eq 0 ];then
+		echo -e "\033[32m $ip is up\e[0m"
+    else
+		echo -e "\033[31m $ip is down\033[0m"
+    fi
 done
